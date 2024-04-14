66a2dd5b9a856d06a7d4cb5cef51836f2c1855a4
Ahmed Ali <ahmedali70x@gmail.com>
2024-04-14 18:09:51 +0100

    load vcard 1

--- function2/src/main.js
+++ function2/src/main.js
@@ -7,7 +7,7 @@ const storage = new Storage(client);
 client
     .setEndpoint('https://cms.itexpertnow.com/v1')
     .setProject('66158c1e000f88577e01')
-    .setKey('f9f2aecebec9de276c5e9a75301664b3f06f6f73725a033bc44260ae60428065dc6ef18fba082232f84064502c4fa7eddf8067f8eb8076c2d122dcc16f6c570bcb33c5ba6eb71177b7adcbe680075b3894d0240980f918d0c33aff9e6fb23eaf21b25529221941f148455c61d91242a37bc7f06ceddd19f18c738ac98b8140fc'); // Your secret API key
+        .setKey('f9f2aecebec9de276c5e9a75301664b3f06f6f73725a033bc44260ae60428065dc6ef18fba082232f84064502c4fa7eddf8067f8eb8076c2d122dcc16f6c570bcb33c5ba6eb71177b7adcbe680075b3894d0240980f918d0c33aff9e6fb23eaf21b25529221941f148455c61d91242a37bc7f06ceddd19f18c738ac98b8140fc'); // Your secret API key
 
 
 
@@ -18,16 +18,10 @@ export default async ({ req, res, log, error }) => {
 
   // The `req` object contains the request data
   if (req.method === 'GET') {
-    const vcf = storage.getFileDownload(
+    storage.getFileDownload(
       '661970c40027f908ba28',
       'd5132d6d-34f9-4e19-a4bc-a318df3b8ff6',
   );
-  load_link = "https://cms.itexpertnow.com/v1/storage/buckets/661970c40027f908ba28/files/d5132d6d-34f9-4e19-a4bc-a318df3b8ff6/download?project=66158c1e000f88577e01&mode=admin"
-  return res.send(load_link);
-    
-
-
-
   }
 
   // `res.json()` is a handy helper for sending JSON
