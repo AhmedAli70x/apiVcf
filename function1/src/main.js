8de0e3fb5336f6223e73f3a786a64b11838bae2a
Ahmed Ali <ahmedali70x@gmail.com>
2024-04-17 20:26:19 +0100

    function1 api

--- function1/src/main.js
+++ function1/src/main.js
@@ -1,5 +1,18 @@
-import { Client } from 'node-appwrite';
+import { Client , Databases, Storage } from 'node-appwrite';
 var fs = require('fs');
+var vCardsJS = require('vcards-js');
+
+const client = new Client();
+const databases = new Databases(client);
+const functions = new Storage(client);
+var vCard = vCardsJS();
+
+client
+    .setEndpoint('https://cms.itexpertnow.com/v1')
+    .setProject('66158c1e000f88577e01')
+    .setKey('f9f2aecebec9de276c5e9a75301664b3f06f6f73725a033bc44260ae60428065dc6ef18fba082232f84064502c4fa7eddf8067f8eb8076c2d122dcc16f6c570bcb33c5ba6eb71177b7adcbe680075b3894d0240980f918d0c33aff9e6fb23eaf21b25529221941f148455c61d91242a37bc7f06ceddd19f18c738ac98b8140fc'); // Your secret API key
+
+
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
@@ -14,14 +27,32 @@ export default async ({ req, res, log, error }) => {
   if (req.method === 'POST') {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
-    return res.json(req.body);   
+    
+    
+    //set properties
+  vCard.firstName = req.body.firstName;
+  vCard.middleName = req.body.middleName;
+  vCard.lastName = req.body.lastName;
+  vCard.organization = req.body.organization;
+  vCard.photo.attachFromUrl('https://1drv.ms/i/s!ArOcLYH4RemJo01S7Hv9NGxomCwC?e=Gg6ffb', 'JPEG');
+  vCard.workPhone = req.body.workPhone;
+  vCard.title = req.body.title;
+  vCard.url = req.body.url;
+  vCard.note = req.body.note;
+
+  const promise = storage.createFile(
+    '661970c40027f908ba28',
+    ID.unique(),
+    sdk.InputFile.fromPath("./eric-nesser2.vcf", "eric-nesser2.vcf")
+);
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
+ 
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  // return res.json({
-  //   motto: 'Build like a team of hundreds_',
-  //   learn: 'https://appwrite.io/docs',
-  //   connect: 'https://appwrite.io/discord',
-  //   getInspired: 'https://builtwith.appwrite.io',
-  // });
+ 
 };
